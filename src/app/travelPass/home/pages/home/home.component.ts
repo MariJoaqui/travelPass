b5d812e07ccea8716f0df3f27ecbc8c378b4ee95
Maria Joaquina <morenoaulaciomariajoaquina@gmail.com>
2023-07-10 10:40:17 -0400

    Completada la información de la página principal.

--- src/app/travelPass/home/pages/home/home.component.ts
+++ src/app/travelPass/home/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 // Interfaces
-import { CardItems, NavItems } from 'src/app/shared/interfaces/interfaces';
+import { NavItems } from 'src/app/shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-home',
@@ -18,31 +18,31 @@ export class HomeComponent {
   ];
 
   // Cartas de ofertas de ejemplos
-  cardItems: CardItems[] = [
-    // {
-    //   image: '../../../../../assets/img/image/carta-1.jpg',
-    //   title: 'Plan Margarita',
-    //   description: 'Disfruta de 3 días y 2 noches en esta hermosa isla con toda tu familia.',
-    //   limit: '5 personas',
-    //   price: '450',
-    //   ubication: 'Margarita, Nueva Esparta.'
-    // },
-    // {
-    //   image: '../../../../../assets/img/image/carta-2.jpg',
-    //   title: 'Plan Margarita',
-    //   description: 'Disfruta de 3 días y 2 noches en esta hermosa isla con toda tu familia.',
-    //   limit: '5 personas',
-    //   price: '450',
-    //   ubication: 'Margarita, Nueva Esparta.'
-    // },
-    // {
-    //   image: '../../../../../assets/img/image/carta-3.jpg',
-    //   title: 'Plan Margarita',
-    //   description: 'Disfruta de 3 días y 2 noches en esta hermosa isla con toda tu familia.',
-    //   limit: '5 personas',
-    //   price: '450',
-    //   ubication: 'Margarita, Nueva Esparta.'
-    // }
+  cardItems: any[] = [
+    {
+      image: '../../../../../assets/img/image/1.jpg',
+      title: 'Casa en Morrocoy',
+      description: 'Disfruta de 3 días y 2 noches en esta hermosa casita con toda tu familia...',
+      limit: '5 personas',
+      price: '450$',
+      ubication: 'Falcón, Venezuela.'
+    },
+    {
+      image: '../../../../../assets/img/image/2.jpg',
+      title: 'Plan Tropical',
+      description: 'Te proporcionamos una habitación con baño y cocina al estilo tropical...',
+      limit: '2 personas',
+      price: '180$',
+      ubication: 'Margarita, Venezuela.'
+    },
+    {
+      image: '../../../../../assets/img/image/3.jpg',
+      title: 'Habitación en Adicora',
+      description: 'Cuarto o habitación para 2 personas por 2 noches y 3 días. te brindamos...',
+      limit: '2 personas',
+      price: '120$',
+      ubication: 'Falcón, Venezuela.'
+    }
   ];
 
 }
